fdd4162e2c58ec6057545194da7e255c7ffab12d
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2023-03-17 11:23:54 +0100

    No change

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { api } from './api';
+// import { validateRequest } from './middleware/validation.middleware';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true })); // set to false when done with project
+// app.use(validateRequest);
 
 app.use('/api', api);
 
