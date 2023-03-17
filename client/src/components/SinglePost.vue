963a73ad5a660955be480e6b88b989eaa37059af
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2023-03-17 11:28:28 +0100

    Removed clients from git repo as development has not started

--- client/src/components/SinglePost.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import type {Post} from "@/interfaces/Post";
-defineProps<{post: Post;}>();
-</script>
-
-<template>
-    <p class="font-black">{{ post.id }}</p>
-    <p>{{ post.title }}</p>
-</template>
